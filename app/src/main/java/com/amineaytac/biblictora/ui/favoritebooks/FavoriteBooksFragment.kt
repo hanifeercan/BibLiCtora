8afb6892de07e5ccae7c2c7273740b01bedebb02
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2025-01-30 15:49:16 +0300

    Create FavoriteBooksFragment

---
+++ app/src/main/java/com/amineaytac/biblictora/ui/favoritebooks/FavoriteBooksFragment.kt
@@ -0,0 +1,6 @@
+package com.amineaytac.biblictora.ui.favoritebooks
+
+import androidx.fragment.app.Fragment
+import com.amineaytac.biblictora.R
+
+class FavoriteBooksFragment : Fragment(R.layout.fragment_favorite_books)
