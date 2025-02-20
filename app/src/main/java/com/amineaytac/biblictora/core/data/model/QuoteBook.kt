59bc9d1d64b0d55376e78592117ec012def9e236
amine <aytacamine633@gmail.com>
2025-02-20 14:22:19 +0300

    Edit QuoteBook

--- app/src/main/java/com/amineaytac/biblictora/core/data/model/QuoteBook.kt
+++ app/src/main/java/com/amineaytac/biblictora/core/data/model/QuoteBook.kt
@@ -1,9 +1,9 @@
 package com.amineaytac.biblictora.core.data.model
 
-data class QuoteBook (
+data class QuoteBook(
     val id: Int,
     val authors: String,
     val title: String,
     val image: String,
-    val quotesList: List<String>
+    val quotesList: List<QuoteItem>
 )
