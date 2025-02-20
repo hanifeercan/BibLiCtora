a0208fe4bb6663e2b96c67342d0efb6e1dab189a
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2025-02-20 17:40:16 +0300

    Create QuotesBookAdapter

---
+++ app/src/main/java/com/amineaytac/biblictora/ui/quotes/QuotesBookAdapter.kt
@@ -0,0 +1,62 @@
+package com.amineaytac.biblictora.ui.quotes
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.amineaytac.biblictora.R
+import com.amineaytac.biblictora.core.data.model.QuoteBook
+import com.amineaytac.biblictora.databinding.ItemQuoteListBinding
+import com.squareup.picasso.Picasso
+
+class QuotesBookAdapter(
+    private val onBookClickListener: (item: QuoteBook, ImageView) -> Unit
+) : ListAdapter<QuoteBook, QuotesBookAdapter.ViewHolder>(
+    COMPARATOR
+) {
+
+    inner class ViewHolder(private val binding: ItemQuoteListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: QuoteBook, position: Int) = with(binding) {
+
+            Picasso.get().load(item.image).error(R.drawable.ic_detail_book).into(ivBook)
+
+            tvName.text = item.title
+            ivBook.setOnClickListener {
+                getItem(position)?.let {
+                    onBookClickListener.invoke(it, ivBook)
+                }
+            }
+        }
+    }
+
+    companion object {
+        private val COMPARATOR = object : DiffUtil.ItemCallback<QuoteBook>() {
+            override fun areItemsTheSame(
+                oldItem: QuoteBook, newItem: QuoteBook
+            ): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(
+                oldItem: QuoteBook, newItem: QuoteBook
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemQuoteListBinding.inflate(LayoutInflater.from(parent.context))
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = getItem(position)
+        item?.let {
+            holder.bind(item, position)
+        }
+    }
+}
