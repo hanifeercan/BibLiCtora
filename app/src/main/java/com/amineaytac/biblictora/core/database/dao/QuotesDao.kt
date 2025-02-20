ef99c3cc625634bd270f1487d7994d231a885d24
amine <aytacamine633@gmail.com>
2025-02-20 14:23:51 +0300

    Edit QuotesDao

--- app/src/main/java/com/amineaytac/biblictora/core/database/dao/QuotesDao.kt
+++ app/src/main/java/com/amineaytac/biblictora/core/database/dao/QuotesDao.kt
@@ -1,13 +1,18 @@
 package com.amineaytac.biblictora.core.database.dao
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Transaction
+import com.amineaytac.biblictora.core.data.model.QuoteItem
 import com.amineaytac.biblictora.core.data.model.ReadingBook
 import com.amineaytac.biblictora.core.database.entity.QuotesEntity
+import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
+import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -17,7 +22,7 @@ interface QuotesDao {
     suspend fun addOrUpdateBook(quotesEntity: QuotesEntity)
 
     @Query("SELECT quotes_list FROM quotes_table WHERE id = :bookId")
-    suspend fun getQuotesForBook(bookId: Int): List<String>?
+    suspend fun getQuotesForBook(bookId: Int): String?
 
     @Query("SELECT * FROM quotes_table WHERE id = :bookId")
     fun getQuoteBook(bookId: Int): LiveData<QuotesEntity>
@@ -25,52 +30,71 @@ interface QuotesDao {
     @Query("SELECT * FROM quotes_table")
     fun getQuoteBooks(): Flow<List<QuotesEntity>>
 
-    @Transaction
-    suspend fun addQuoteToBook(readingBook: ReadingBook, newQuote: String) {
-        val currentQuotes = getQuotesForBook(readingBook.id)
+    suspend fun addQuoteToBook(readingBook: ReadingBook, newQuoteText: String) {
+        val currentQuotesJson = getQuotesForBook(readingBook.id)
 
-        if (currentQuotes == null) {
-            val newQuotesEntity = QuotesEntity(
-                id = readingBook.id,
-                title = readingBook.title,
-                authors = readingBook.authors,
-                image = readingBook.image,
-                quotesList = listOf(newQuote)
-            )
-            addOrUpdateBook(newQuotesEntity)
-        } else {
-            val updatedList = currentQuotes.toMutableList().apply {
-                add(newQuote)
+        val currentQuotes = currentQuotesJson?.let {
+            val type = object : TypeToken<List<QuoteItem>>() {}.type
+            try {
+                Gson().fromJson<List<QuoteItem>>(it, type)
+            } catch (e: JsonSyntaxException) {
+                Log.e("QuoteDao", "Json dönüştürme hatası: ${e.message}")
+                null
             }
+        } ?: emptyList()
 
-            val updatedBook = QuotesEntity(
-                id = readingBook.id,
-                title = readingBook.title,
-                authors = readingBook.authors,
-                image = readingBook.image,
-                quotesList = updatedList
-            )
-            addOrUpdateBook(updatedBook)
+        val updatedQuotes = currentQuotes.toMutableList()
+        val newQuote = QuoteItem(id = updatedQuotes.size, quote = newQuoteText)
+
+        if (updatedQuotes.none {
+                it.quote.trim().equals(newQuote.quote.trim(), ignoreCase = true)
+            }) {
+            updatedQuotes.add(newQuote)
         }
+
+        val updatedBook = QuotesEntity(
+            id = readingBook.id,
+            title = readingBook.title,
+            authors = readingBook.authors,
+            image = readingBook.image,
+            quotesList = Gson().toJson(updatedQuotes)
+        )
+        addOrUpdateBook(updatedBook)
     }
 
     @Transaction
     suspend fun deleteQuoteFromBook(bookId: Int, quoteToRemove: String) {
-        val currentQuotes = getQuotesForBook(bookId)
-        currentQuotes?.let {
-            val updatedList = it.filter { quote -> quote != quoteToRemove }
-            updateQuotesList(bookId, updatedList)
-        }
-    }
 
-    @Query("UPDATE quotes_table SET quotes_list = :updatedList WHERE id = :bookId")
-    suspend fun updateQuotesList(bookId: Int, updatedList: List<String>) {
+        val currentQuotesJson = getQuotesForBook(bookId)
+        val currentQuotes: List<QuoteItem>? = currentQuotesJson?.let {
+            Gson().fromJson(it, object : TypeToken<List<QuoteItem>>() {}.type)
+        }
+
+        if (currentQuotes == null || currentQuotes.isEmpty()) {
+            return
+        }
+
+        val updatedList = currentQuotes.filter { quote ->
+            val isKept = !quote.quote.trim().equals(quoteToRemove.trim(), ignoreCase = true)
+            isKept
+        }
+
         if (updatedList.isEmpty()) {
             deleteBookById(bookId)
         } else {
             updateQuotesList(bookId, updatedList)
         }
     }
+
+    @Transaction
+    suspend fun updateQuotesList(bookId: Int, updatedList: List<QuoteItem>) {
+
+        val updatedListJson = Gson().toJson(updatedList)
+        updateQuotesListJson(bookId, updatedListJson)
+    }
+
+    @Query("UPDATE quotes_table SET quotes_list = :updatedListJson WHERE id = :bookId")
+    suspend fun updateQuotesListJson(bookId: Int, updatedListJson: String)
 
     @Query("DELETE FROM quotes_table WHERE id = :bookId")
     suspend fun deleteBookById(bookId: Int)
