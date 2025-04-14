71303b4a4421def7f2181572e7a2195d4d970d20
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2025-04-14 21:33:01 +0300

    Create EpubViewerFragment

---
+++ app/src/main/java/com/amineaytac/biblictora/ui/epubviewer/EpubViewerFragment.kt
@@ -0,0 +1,8 @@
+package com.amineaytac.biblictora.ui.epubviewer
+
+import androidx.fragment.app.Fragment
+import com.amineaytac.biblictora.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class EpubViewerFragment : Fragment(R.layout.fragment_epub_viewer)
