bd5333c6c859345a534670c944763adcb6ede7b5
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2025-04-14 21:51:19 +0300

    Add getId function

--- app/src/main/java/com/amineaytac/biblictora/ui/epubviewer/EpubViewerViewModel.kt
+++ app/src/main/java/com/amineaytac/biblictora/ui/epubviewer/EpubViewerViewModel.kt
@@ -1,8 +1,11 @@
 package com.amineaytac.biblictora.ui.epubviewer
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.amineaytac.biblictora.core.data.repo.BookRepository
+import com.amineaytac.biblictora.core.domain.mybooks.GetIdUseCase
 import com.amineaytac.biblictora.core.domain.mybooks.UpdateLastPageUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -12,7 +15,11 @@ import javax.inject.Inject
 class EpubViewerViewModel @Inject constructor(
     private val updateLastPageUseCase: UpdateLastPageUseCase,
     private val bookRepository: BookRepository,
+    private val getIdUseCase: GetIdUseCase
 ) : ViewModel() {
+
+    private val _id = MutableLiveData<Int>()
+    val id: LiveData<Int> get() = _id
 
     suspend fun getLastPage(filePath: String): Int {
         return bookRepository.getLastPage(filePath)
@@ -21,6 +28,12 @@ class EpubViewerViewModel @Inject constructor(
     fun updateLastPage(filePath: String, lastPage: Int) {
         viewModelScope.launch {
             updateLastPageUseCase(filePath, lastPage)
+        }
+    }
+
+    fun getId(filePath: String) {
+        viewModelScope.launch {
+            _id.value = getIdUseCase.invoke(filePath)
         }
     }
 }
