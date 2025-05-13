ea19dd3ae29351d44769ebe044ea3a2957df5a8b
Hanife Ercan <102989222+hanifeercan@users.noreply.github.com>
2025-05-13 20:13:35 +0300

    Edit NetworkListener

--- app/src/main/java/com/amineaytac/biblictora/core/network/NetworkListener.kt
+++ app/src/main/java/com/amineaytac/biblictora/core/network/NetworkListener.kt
@@ -1,5 +1,21 @@
 package com.amineaytac.biblictora.core.network
 
+import android.content.Context
+import com.amineaytac.biblictora.R
+import com.yagmurerdogan.toasticlib.Toastic
+
 interface NetworkListener {
     fun onNetworkStateChanged(isConnected: Boolean)
+
+    fun onInternetDisconnected(context: Context) {
+        Toastic.toastic(
+            context = context,
+            message = context.getString(R.string.check_internet_connection),
+            duration = Toastic.LENGTH_SHORT,
+            type = Toastic.ERROR,
+            isIconAnimated = true
+        ).show()
+    }
+
+    fun onInternetConnected(context:Context)
 }
